fab438075b3ade847a6ab4c5888d8b4c125f2811
Johan Dynnesen <johandynnesen@gmail.com>
2021-04-15 15:01:11 +0200

    Add cors and custom middleware log

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const db = require("./lib/db");
+const cors = require("cors");
 
 /*
   We create an express app calling
@@ -13,7 +14,13 @@ const app = express();
   https://expressjs.com/en/guide/using-middleware.html
 */
 app.use(express.json());
-
+app.use(cors());
+app.use((req, res, next) => {
+const { method, url} = req;
+console.log("My custom middleware");
+  console.log(`${method} ${url}`);
+  next();
+});
 /*
   Endpoint to handle GET requests to the root URI "/"
 */
@@ -24,8 +31,96 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/posts", (req, res) => {
+  db.findAll().then(posts => {
+  res.status(200);
+  res.json(posts);
+  }).catch(error => {
+    res.status(500);
+    res.json({
+      error: `Internal ServerError: ${error}`,
+    
+  })
+});
+});
+
+app.post("/posts", (req, res) => {
+db.insert(req.body)
+.then(newPost => {
+  res.status(200);
+  res.json(newPost);
+})
+.catch((error) => {
+  res.status(500);
+  res.json({
+    error: "Internal server error",
+  });
+});
+ 
+});
+
+app.get("/posts/:id", (req, res) => {
+ let id = req.params.id;
+//  res.json(idVariable);
+//   console.log(idVariable);
+  db.findById(id)
+  .then(foundPost => {
+    res.json(foundPost);
+    res.status(200);
+    })
+    .catch((error) => {
+  res.status(500);
+  res.json({
+    error: "Internal server error",
+  });      
+    });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let idVar = req.params.id;
+  db.updateById(idVar, req.body)
+  .then(foundPost => {
+    res.json(foundPost);
+    res.status(200);
+    })
+    .catch((error) => {
+      res.status(500);
+      res.json({
+        error: "Internal server error",
+      });   
+});
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let idVar = req.params.id;
+  db.deleteById(idVar).then((post) => {
+    res.status(204);
+    res.json(post);
+    console.log(`Deleted post number ${idVar} `);
+  }) .catch((error) => {
+    res.status(500);
+    res.json({
+      error: "Internal server error",
+    });   
+});
+});
+  
+
+//   // .then(newPost => {
+    
+    
+//   //   })
+//   //   .catch((error) => {
+//   //     res.status(500);
+//   //     res.json({
+//   //       error: "Internal server error",
+//   //     });  
+// });
+
 /*
-  We have to start the server. We make it listen on the port 4000
+
+
+We have to start the server. We make it listen on the port 4000
 
 */
 app.listen(4000, () => {
